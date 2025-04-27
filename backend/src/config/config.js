dcb6fae28d16ec74298a20bdd4455174abb0f035
Gisele-123 <giselemigisha53@gmail.com>
2025-04-27 10:56:54 +0200

    configurations finished success

---
+++ backend/src/config/config.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+module.exports={
+    PORT: process.env.PORT || 5000,
+    DB_USER: process.env.DB_USER,
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT || 5432,
+    DB_NAME: process.env.DB_NAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    JWT_SECRET: process.env.JWT_SECRET
+}
